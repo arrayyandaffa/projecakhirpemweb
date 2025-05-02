ba37662a93740679952db2637108f0d2843c5dcc
arrayyandaffa <arrayyandaffa30@gmail.com>
2025-05-03 02:00:27 +0700

    Add files via upload

---
+++ web-sekolah/app/Models/SiswaKelas3.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SiswaKelas3 extends Model
+{
+    protected $table = 'siswa_kelas_3';
+    public $timestamps = false;
+}
