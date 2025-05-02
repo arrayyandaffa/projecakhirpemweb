bbefc1725e55d9e8872e134e11f60b0504ab7cc0
arrayyandaffa <arrayyandaffa30@gmail.com>
2025-05-03 02:11:49 +0700

    Update alumni.blade.php
    edit tampilan html direktori/alumni

--- web-sekolah/resources/views/direktori/alumni.blade.php
+++ web-sekolah/resources/views/direktori/alumni.blade.php
@@ -1,5 +1,6 @@
+<!--sebelum-->
 <!-- resources/views/profil/visi-misi.blade.php -->
-@extends('layouts.app')
+<!-- @extends('layouts.app')
 
 @section('content')
     <div class="container mt-5">
@@ -9,4 +10,35 @@
     </div>
 @endsection
 
+sesudah -->
+@extends('layouts.app')
 
+@section('content')
+<div class="container mt-5">
+    <h2 class="mb-4">Data Alumni</h2>
+    <table class="table table-bordered table-striped">
+        <thead class="table-dark">
+            <tr>
+                <th>No</th>
+                <th>Nama</th>
+                <th>NISN</th>
+                <th>Jenis Kelamin</th>
+                <th>Tanggal Lahir</th>
+                <th>Tahun Lulus</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($data as $index => $item)
+                <tr>
+                    <td>{{ $index + 1 }}</td>
+                    <td>{{ $item->nama }}</td>
+                    <td>{{ $item->nisn }}</td>
+                    <td>{{ $item->jenis_kelamin }}</td>
+                    <td>{{ $item->tanggal_lahir }}</td>
+                    <td>{{ $item->tahun_lulus }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+@endsection
